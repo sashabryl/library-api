e98c7da2c9cb3af36ad2f955239595eec389c526
Olexander Bryl <edlrian814@gmail.com>
2023-11-22 20:20:13 +0200

    Add UserRegisterView(CreateAPIView)

--- user/views.py
+++ user/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+from django.contrib.auth import get_user_model
+from rest_framework import generics
 
-# Create your views here.
+from user.serializers import UserCreateSerializer
+
+
+class UserRegisterView(generics.CreateAPIView):
+    serializer_class = UserCreateSerializer
+    queryset = get_user_model().objects.all
+    authentication_classes = []
+    permission_classes = []
