f0d229a48eae9304c146f989acd49aab7bb74fc7
Olexander Bryl <edlrian814@gmail.com>
2023-11-22 20:42:59 +0200

    Configure simple-jwt by changing AUTH_HEADER_NAME to 'HTTP_Authorize' & prolonging access token lifetime to 1 day

--- library_api_service/settings.py
+++ library_api_service/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/4.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
-
+from datetime import timedelta
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -133,3 +133,9 @@ REST_FRAMEWORK = {
         "rest_framework_simplejwt.authentication.JWTAuthentication",
     ]
 }
+
+SIMPLE_JWT = {
+    "ACCESS_TOKEN_LIFETIME": timedelta(days=1),
+    "AUTH_HEADER_NAME": "HTTP_Authorize",
+}
+
