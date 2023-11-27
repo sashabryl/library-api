ab1237b7d4b76593ce906fce5b338e3e121c15ff
Olexander Bryl <edlrian814@gmail.com>
2023-11-27 12:32:07 +0200

    Install & configure celery

--- book/views.py
+++ book/views.py
@@ -55,7 +55,9 @@ class BorrowViewSet(
         queryset = Borrowing.objects.all()
 
         if self.action == "list" and not self.request.user.is_staff:
-            return queryset.filter(user=self.request.user).select_related("user", "book")
+            return queryset.filter(user=self.request.user).select_related(
+                "user", "book"
+            )
 
         user_id = self.request.query_params.get("user_id")
         is_active = self.request.query_params.get("is_active")
@@ -79,7 +81,12 @@ class BorrowViewSet(
     def perform_create(self, serializer):
         serializer.save(user=self.request.user)
 
-    @action(methods=["GET"], detail=True, url_path="return", permission_classes=[IsAdminUser])
+    @action(
+        methods=["GET"],
+        detail=True,
+        url_path="return",
+        permission_classes=[IsAdminUser],
+    )
     def return_book(self, request, pk=None):
         borrowing = self.get_object()
         if borrowing.actual_return_date:
