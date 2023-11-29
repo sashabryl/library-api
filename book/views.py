adbde9cc937d25b770caefeac7b3353666851cf1
Olexander Bryl <edlrian814@gmail.com>
2023-11-29 18:54:21 +0200

    Documentize BorrowViewSet

--- book/views.py
+++ book/views.py
@@ -4,6 +4,7 @@ import os
 import stripe
 from django.db.models import Q
 from django.http import HttpResponseRedirect
+from drf_spectacular.utils import extend_schema, OpenApiParameter
 from rest_framework import viewsets
 from rest_framework.decorators import action
 from rest_framework.mixins import (
@@ -94,6 +95,11 @@ class BorrowViewSet(
         serializer.save(user=self.request.user)
 
     def create(self, request, *args, **kwargs):
+        """
+        If the user does not have unpaid payment,
+        redirects to a stripe payment session.
+        Otherwise, return 403.
+        """
         if Payment.objects.filter(
             borrowing__in=request.user.borrowings.all(), status="PENDING"
         ).exists():
@@ -118,6 +124,12 @@ class BorrowViewSet(
         permission_classes=[IsAdminUser],
     )
     def return_book(self, request, pk=None):
+        """
+        Sets the current date as actual_return_date of the borrowing.
+        If it is not null already, returns status code 400.
+        If the actual_return_date turns out to be later than expected,
+        a fine payment is created.
+        """
         borrowing = self.get_object()
         if borrowing.actual_return_date:
             return Response(
@@ -142,6 +154,29 @@ class BorrowViewSet(
             f"here is their fine: "
             f"{create_payment(request, borrowing, 'FINE')}"
         )
+
+    @extend_schema(
+        parameters=[
+            OpenApiParameter(
+                name="is_active",
+                description=(
+                        "Filter by case-insensitive is_active "
+                        "(bool) (ex. ?is_active=TRUe)"
+                ),
+                required=False,
+                type=str,
+            ),
+            OpenApiParameter(
+                name="user_id",
+                description="Filter by id of a user (ex. ?user_id=2)",
+                required=False,
+                type=int,
+            ),
+        ]
+    )
+    def list(self, request, *args, **kwargs):
+        """Filtering is for superusers only."""
+        return super().list(request, *args, **kwargs)
 
 
 class PaymentViewSet(
